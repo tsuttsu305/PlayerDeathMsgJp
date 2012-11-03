63a1e4f83d32c8e193d91a237e875c93dff96db1
tsuttsu305 <Administrator@127.0.0.1>
2012-11-04 03:58:08 +0900

    write config.yml

--- src/jp/tsuttsu305/Main.java
+++ src/jp/tsuttsu305/Main.java
@@ -1,5 +1,26 @@
 package jp.tsuttsu305;
 
-public class Main {
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	public static Main plugin;
+	Logger logger = Logger.getLogger("Minecraft");
+
+	public void onEnable(){
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.info(pdfFile.getName() + "version" + pdfFile.getVersion() + " is Enabled");
+
+	}
+
+	public void onDisable(){
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.info(pdfFile.getName() + "version" + pdfFile.getVersion() + " is Disabled");
+
+	}
+	
+	
 
 }
